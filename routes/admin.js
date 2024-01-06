a4404ec4fd3dcebcaba7e7b9c1f72c0ad25019f0
DeepakSehrawat1 <deepaksehrawat150@gmail.com>
2024-01-06 17:23:15 +0530

    starting

---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const productController = require("../controller/product");
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get("/add-product", productController.getproduct);
+
+// /admin/add-product => POST
+router.post("/add-product", productController.addproduct);
+
+module.exports = router;
